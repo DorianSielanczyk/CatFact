8618e4cd9325a38524996cc2641fadffeae6290a
DorianSielanczyk <sielanczykdorian@gmail.com>
2026-09-07 20:22:37 +0200

    refactor: refactor project structure and add API project

---
+++ CatFactNinja/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,14 @@
+﻿namespace CatFact.API.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+
+            return services;
+        }
+    }
+}
---
+++ CatFactNinja/Extensions/WebApplicationExtensions.cs
@@ -0,0 +1,22 @@
+﻿namespace CatFact.API.Extensions
+{
+    public static class WebApplicationExtensions
+    {
+        public static WebApplication ConfigureHttpRequestPipeline(this WebApplication app)
+        {
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.MapControllers();
+
+            return app;
+        }
+    }
+}

820c283e2429e8a38905e3cf0bf4c3c4e2e51898
DorianSielanczyk <sielanczykdorian@gmail.com>
2026-09-09 21:42:59 +0200

    fix: change errors exceptions messenges from EN to PL

--- src/HealthChecks/FactApiHealthCheck.cs
+++ src/HealthChecks/FactApiHealthCheck.cs
@@ -13,19 +13,19 @@ namespace CatFact.API.HealthChecks
             {
                 await factClient.GetFactResponseAsync(cancellationToken);
 
-                return HealthCheckResult.Healthy("CatFact API is responding correctly.");
+                return HealthCheckResult.Healthy("CatFact API działa poprawnie.");
             }
             catch (OperationCanceledException ex) when (!cancellationToken.IsCancellationRequested)
             {
-                return HealthCheckResult.Degraded("CatFact API request timed out.", ex);
+                return HealthCheckResult.Degraded("Żądanie do CatFact API przekroczyło limit czasu.", ex);
             }
             catch (HttpRequestException ex)
             {
-                return HealthCheckResult.Degraded($"CatFact API network error: {ex.Message}");
+                return HealthCheckResult.Degraded($"Wystąpił błąd sieci podczas komunikacji z CatFact API: {ex.Message}");
             }
             catch (Exception ex)
             {
-                return HealthCheckResult.Unhealthy("Failed to connect to CatFact API.", ex);
+                return HealthCheckResult.Unhealthy("Nie udało się połączyć z CatFact API.", ex);
             }
         }
     }
